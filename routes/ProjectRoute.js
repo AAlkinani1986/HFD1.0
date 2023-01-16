ab0bd5fe7ff94e4c657ee90b4f28bebc36d60174
AAlkinani1986 <90129610+AAlkinani1986@users.noreply.github.com>
2023-01-16 15:37:21 +1100

    install dependencies

--- routes/ProjectRoute.js
+++ routes/ProjectRoute.js
@@ -1,30 +1,30 @@
-import { Router } from "express";
-import userController from "../controllers/userController.js";
+import { Router } from 'express'
+import userController from '../controllers/userController.js'
 
-var router = Router();
-router.get("/", function (req, res) {
-  userController.login(req, res);
-  res.render("user/login", {
+var router = Router()
+router.get('/', function (req, res) {
+  userController.login(req, res)
+  res.render('user/login', {
     // additional locals, a custom layout, or other options can be defined here
-  });
-});
-router.get("/newUser", function (req, res) {
-  userController.register(req, res);
-  res.render("user/register", {
+  })
+})
+router.get('/newUser', function (req, res) {
+  userController.register(req, res)
+  res.render('user/register', {
     // additional locals, a custom layout, or other options can be defined here
-  });
-});
+  })
+})
 
-router.get("/newDoctor", function (req, res) {
-  userController.register(req, res);
-  res.render("doctor/doctor", {
+router.get('/newDoctor', function (req, res) {
+  userController.register(req, res)
+  res.render('doctor/newDoctor', {
     // additional locals, a custom layout, or other options can be defined here
-  });
-});
-router.get("/doctorProfile", function (req, res) {
-  userController.register(req, res);
-  res.render("doctor/Profile", {
+  })
+})
+router.get('/doctorProfile', function (req, res) {
+  userController.register(req, res)
+  res.render('doctor/Profile', {
     // additional locals, a custom layout, or other options can be defined here
-  });
-});
-export default router;
+  })
+})
+export default router
