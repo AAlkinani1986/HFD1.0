20a0c8f89170b53c8cf29165b0f5f39fd7c9c6e4
Harshpatel24197 <harshpatel24197@gmail.com>
2023-01-19 13:56:28 +1100

    update

--- routes/clinic/index.js
+++ routes/clinic/index.js
@@ -1,9 +1,12 @@
 import { Router } from 'express'
 
 import { registrationClinic } from './registration.js'
+import { Profile } from './profile.js'
 
 const router = Router()
 export function clinicRoutes(params) {
   router.use(registrationClinic(params))
+  router.use(Profile(params))
   return router
+
 }
---
+++ routes/clinic/profile.js
@@ -0,0 +1,14 @@
+import { Router } from 'express'
+import passport from 'passport'
+
+const router = Router()
+
+export function Profile() {
+  router.get('/profile', function (req, res) {
+    res.render('clinic//profile', {
+      page: 'new profile',
+    })
+  })
+
+  return router
+}
