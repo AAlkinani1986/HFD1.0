3e13b5eacf4f6d400160254be6ee4c6fcd6e9e59
Jo <s222186709@deakin.edu.au>
2023-01-27 23:55:41 +1100

    Added recipes ejs

--- public/js/patient/recipes.js
+++ public/js/patient/recipes.js
@@ -1,1 +1,23 @@
-document.getElementById('recipes').style.color = 'red'
+document.getElementById('recipes').style.color = 'pink'
+let searchButton = document.querySelector("#search")
+
+//Add an event listener to the button that runs the function sendApiRequest when it is clicked
+searchButton.addEventListener("click", ()=>{
+  console.log("button pressed")
+  sendApiRequest()
+})
+
+
+//An asynchronous function to fetch data from the API.
+async function sendApiRequest(){
+  let APP_ID = ""
+  let API_KEY = ""
+  let response = await fetch(``);
+  console.log(response)
+}
+
+
+//function that does something with the data received from the API. The name of the function should be customized to whatever you are doing with the data
+function useApiData(data){
+
+}
