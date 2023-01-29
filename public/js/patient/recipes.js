9c75d468c1e3eba5af9f250d5993a49b7a5f616a
shamail786 <haider.shamail68@gmail.com>
2023-01-29 17:17:38 +1100

    Added Patients List to Doctor Page

--- public/js/patient/recipes.js
+++ public/js/patient/recipes.js
@@ -1,22 +1,24 @@
-document.getElementById('recipes').style.color = 'green'
-let searchButton = document.querySelector("#search")
+document.getElementById("recipes").style.color = "green";
+let searchButton = document.querySelector("#search");
 
 //Add an event listener to the button that runs the function sendApiRequest when it is clicked
 searchButton.addEventListener("click", () => {
-  console.log("button pressed")
-  sendApiRequest()
-})
+  console.log("button pressed");
+  sendApiRequest();
+});
 
 //An asynchronous function to fetch data from the API.
 async function sendApiRequest() {
-  let APP_ID = "d5c69d5f"
-  let API_KEY = "9347c2e9ad93b8aaa1541a7b5da990bf"
-  let response = await fetch(`https://api.edamam.com/search?app_id=${APP_ID}&app_key=${API_KEY}&q=chicken`);
-  console.log(response)
+  let APP_ID = "d5c69d5f";
+  let API_KEY = "9347c2e9ad93b8aaa1541a7b5da990bf";
+  let response = await fetch(
+    `https://api.edamam.com/search?app_id=${APP_ID}&app_key=${API_KEY}&q=chicken`
+  );
+  console.log(response);
 
-  let data = await response.json()
-  console.log(data)
-  useApiData(data)
+  let data = await response.json();
+  console.log(data);
+  useApiData(data);
 }
 
 //function that does something with the data received from the API. The name of the function should be customized to whatever you are doing with the data
@@ -29,5 +31,5 @@ function useApiData(data) {
                  <p class="card-text">Source: ${data.hits[0].recipe.source}</p>
                  <a href="${data.hits[0].recipe.url}" class="btn btn-primary"> Go somewhere </a>
                </div> 
-             </div>`
+             </div>`;
 }
--- routes/Doctor/doctorPatients.js
+++ routes/Doctor/doctorPatients.js
@@ -1,12 +1,21 @@
 import { Router } from "express";
 import passport from "passport";
+import { patientController } from "../../controllers/patientController.js";
 
 const router = Router();
 
 export function doctorPatients() {
-  router.get("/Patient", function (req, res) {
+  router.get("/Patient", async (req, res, next) => {
+    const patient = await patientController.getPatients();
+    const patientList = await Promise.all(
+      patient.map(async (patient) => {
+        const patientData = patient.toJSON();
+        return patientData;
+      })
+    );
     res.render("doctor/patient", {
       page: "profile doctor",
+      patient: patientList,
     });
   });
 
