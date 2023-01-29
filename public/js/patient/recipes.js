0ec21a4deb8258c797f05b04a71d7f2f3f6d2bdf
Jo <s222186709@deakin.edu.au>
2023-01-29 13:29:37 +1100

    revised recipes.ejs

--- public/js/patient/recipes.js
+++ public/js/patient/recipes.js
@@ -1,6 +1,52 @@
-document.getElementById('recipes').style.color = 'green'
-let searchButton = document.querySelector("#search")
+// document.getElementById('recipes').style.color = 'green'
+// let searchButton = document.querySelector("#search")
 
+const searchForm = document.querySelector('form');
+const searchResultDiv = document.querySelector('.search-result');
+const container = document.querySelector('.container');
+let searchQuery = '';
+const APP_ID = "d5c69d5f";
+const APP_key = "9347c2e9ad93b8aaa1541a7b5da990bf";
+// console.log(container)
+searchForm.addEventListener('submit', (e) => {
+  e.preventDefault();
+  searchQuery = e.target.querySelector('input').value;
+  fetchAPI();
+})
+
+async function fetchAPI() {
+  const baseURL = `https://api.edamam.com/search?q=${searchQuery}&app_id=${APP_ID}&app_key=${APP_key}&from=0&to=20`;
+  const response = await fetch(baseURL);
+  const data = await response.json();
+  generateHTML(data.hits)
+  console.log(data);
+}
+
+function generateHTML(results) {
+  container.classList.remove('initial');
+  let generatedHTML = '';
+  results.map(result => {
+    generatedHTML += `
+      <div class="item">
+        <img src="${result.recipe.image}" alt="img">
+        <div class="flex-container">
+          <h1 class="title">${result.recipe.label}</h1>
+          <a class="view-btn" target="_blank" href="${result.recipe.url}">View Recipe</a>
+        </div>
+        <p class="item-data">Calories: ${result.recipe.calories.toFixed(2)}</p>
+        <p class="item-data">Diet label: ${result.recipe.dietLabels.length > 0 ? result.recipe.dietLabels : 'No Data Found'}</p>
+        <p class="item-data">Health labels: ${result.recipe.healthLabels}</p>
+        <center> <i class="small material-icons waves-effect waves-light">favorite</i>
+        <a class="btn-floating btn-small waves-effect waves-light pink"><i class="material-icons">add</i></a>
+    
+        </center>
+      </div>
+    `
+  })
+  searchResultDiv.innerHTML = generatedHTML;
+}
+
+/* 
 //Add an event listener to the button that runs the function sendApiRequest when it is clicked
 searchButton.addEventListener("click", () => {
   console.log("button pressed")
@@ -33,3 +79,4 @@ function useApiData(data) {
                </div> 
              </div>`
 }
+*/
