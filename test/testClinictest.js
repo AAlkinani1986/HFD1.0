075db32917385dab08786500d22f7f8f1e8c0658
Harshpatel24197 <harshpatel24197@gmail.com>
2023-02-07 13:10:39 +1100

    update test

--- test/testClinictest.js
+++ test/testClinictest.js
@@ -1,15 +1,67 @@
+import request from 'supertest'
+import { app } from '../app.js'
 import { expect } from 'chai'
-import request from 'request'
+import config from '../server/config/index.js'
+import mongoose from 'mongoose'
+const App = app(config)
 
 describe('ClinicProfile', function () {
   var url = 'http://localhost:3000/clinic/profile'
   //it functions
 
-  it("return status 200 to check works", function(done){
-    request(url, function(error, response, body){
-        expect(response.statusCode).to.equal(200);
+  before((done) => {
+    mongoose.set('strictQuery', true)
+    mongoose.connect(
+      process.env.MONGODB_URI,
+      { useNewUrlParser: true, useUnifiedTopology: true },
+      (err) => {
+        if (err) return done(err)
         done()
-    });
-    done()
+      },
+    )
+  })
+
+  it('return status 302 to check if home page can load ', function (done) {
+    request(App)
+      .get('/clinic/registration')
+      .expect(302)
+      .end((err, res) => {
+        if (err) return done(err)
+        expect(res.header.location).to.equal('/')
+        done()
+      })
+  })
+  it('clinic has profile Cannot view the registration page ', (done) => {
+    request(App)
+      .post('/user/login')
+      .send({ username: 'Harsh1234', password: '12345678' })
+      .expect(302)
+      .expect('set-cookie', /session=.+/)
+      .end((err, res) => {
+        if (err) return done(err)
+        const cookie = res.header['set-cookie']
+        expect(res.headers.location).to.equal('/clinic/registration')
+        request(App)
+          .get('/clinic/registration')
+          .set('Cookie', cookie)
+          .expect(302)
+          .end((err, res) => {
+            expect(res.headers.location).to.equal('/clinic/profile')
+            if (err) return done(err)
+            done()
+          })
+      })
+  })
+  after((done) => {
+    mongoose.disconnect((err) => {
+      if (err) return done(err)
+      done()
+    })
   })
 })
+
+
+
+
+
+ 
