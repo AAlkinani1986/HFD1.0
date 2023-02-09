f387c82ff7e25436c53b4d9a419dd765d0917be8
Jo <s222186709@deakin.edu.au>
2023-02-09 19:26:56 +1100

    Updated recipeController

--- controllers/recipeController.js
+++ controllers/recipeController.js
@@ -1,4 +1,4 @@
-import { recipe } from '../models/recipesModel'
+import { Recipe, recipe } from '../models/recipesModel'
 
 export class recipeController {
   static async findAll() {
@@ -8,4 +8,96 @@ export class recipeController {
       return error
     }
   }
-}
+    /**
+    * Creating a new Recipe
+    *
+    * @param {recipeName} recipeName;
+    * @param {ingredients} ingredients;
+    * @param {calories} calories;
+    *  @param {dailyValue} dailyValue;
+    */
+    static async createRecipe(
+     recipeName,
+     ingredients,
+     calories,
+     dailyValue,
+   ) {
+     try {
+       const recipe = new Recipe();
+       recipe.recipeName = recipeName;
+       recipe.ingredients = ingredients;
+       recipe.calories = calories;
+       recipe.dailyValue = dailyValue;
+       const savedRecipe = await recipe.save();
+       return savedRecipe;
+     } catch (error) {
+       return error;
+     }
+   }
+   static async updateRecipe(
+     recipeName,
+     ingredients,
+     calories,
+     dailyValue,
+   ) {
+     try {
+       await Recipe.findOneAndUpdate(
+         { recipeName: recipeName },
+         {
+           $set: {
+            recipeName,
+            ingredients,
+            calories,
+            dailyValue,
+           },
+         }
+       );
+       return true;
+     } catch (error) {
+       return error;
+     }
+   }
+ 
+   /**
+    * Finding by name
+    * @param {*} name
+    * @returns
+    */
+   static async findByName(name) {
+     try {
+       return Recipe.findByName(name).exec();
+     } catch (error) {
+       return error;
+     }
+   }
+ 
+   static async findOne(Name) {
+     try {
+       return Recipe.findOne({ recipeName: this.name }).exec();
+     } catch (error) {
+       return error;
+     }
+   }
+ 
+   /**
+    *
+    * @param {*} name
+    * @returns a user
+    */
+   static async getResetToken(recipeName) {
+     try {
+       return ResetTokenModel.findOne({ recipeName }).exec();
+     } catch (error) {
+       return error;
+     }
+   }
+   /**
+    * getting all Recipe
+    *
+    * @returns a list of users
+    */
+   static async getRecipes() {
+     return Recipe.find().sort({ createdAt: -1 }).exec();
+   }
+ }
+ 
